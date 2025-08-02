4a48a6cea3869b24ae59a626f4cc6cef52849ec8
Harsh Patel <harshpatel25121999@gmail.com>
2025-08-02 18:16:57 -0500

    Added the hooks

--- react-app/src/Components/TodoList/Todo.jsx
+++ react-app/src/Components/TodoList/Todo.jsx
@@ -1,5 +1,5 @@
 import "../../App.css";
-import { useState } from "react";
+import { useState, useMemo, useCallback } from "react";
 import { v4 as uuidv4 } from "uuid";
 import TodoItem from "./TodoItem";
 
@@ -9,6 +9,16 @@ function Todo() {
   const [editInput, setEditInput] = useState("");
   const [editingId, setEditingId] = useState(null);
 
+  const pendingTodos = useMemo(
+    () => todoList.filter((todo) => !todo.isCompleted),
+    [todoList]
+  );
+
+  const CompletedTodos = useMemo(
+    () => todoList.filter((todo) => todo.isCompleted),
+    [todoList]
+  );
+
   function handleTodoItemChange(event) {
     setTodoItem(event.target.value);
   }
@@ -16,13 +26,13 @@ function Todo() {
     setEditInput(event.target.value);
   }
 
-  function addTodoItem() {
+  const addTodoItem = useCallback(() => {
     if (todoItem.trim().length) {
-      const newItem = { id: uuidv4(), value: todoItem };
+      const newItem = { id: uuidv4(), value: todoItem, isCompleted: false };
       setTodoList([...todoList, newItem]);
       setTodoItem("");
     }
-  }
+  }, [todoItem]);
 
   function editTodo(item) {
     if (editingId != null) {
@@ -38,25 +48,42 @@ function Todo() {
     setTodoList(newList);
   }
 
-  function saveTodo(id) {
-    if (editInput.trim() === "") return;
-    const newList = todoList.map((todo) => {
-      if (todo.id === id) {
-        return { ...todo, value: editInput };
-      }
-      return todo;
-    });
-    setTodoList(newList);
-    setEditingId(null);
-  }
+  const saveTodo = useCallback(
+    (id) => {
+      if (editInput.trim() === "") return;
+      const newList = todoList.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, value: editInput };
+        }
+        return todo;
+      });
+      setTodoList(newList);
+      setEditingId(null);
+    },
+    [editInput]
+  );
 
   function cancelEdit() {
     setEditInput("");
     setEditingId(null);
   }
+
+  function toggleCompleted(id) {
+    if (editingId === id) {
+      alert("Save the current item.");
+      return;
+    }
+    const newList = todoList.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, isCompleted: !todo.isCompleted };
+      }
+      return todo;
+    });
+    setTodoList(newList);
+  }
   return (
     <>
-      <div>
+      <div className="input-container">
         <input
           value={todoItem}
           onChange={handleTodoItemChange}
@@ -64,23 +91,50 @@ function Todo() {
         />
         <button onClick={addTodoItem}>Add</button>
       </div>
-      {todoList.length > 0 && <h2 className="todo-list-title">Todo List</h2>}
-      <div id="todo-list">
-        <ul>
-          {todoList.map((item) => (
-            <TodoItem
-              key={item.id}
-              item={item}
-              editingId={editingId}
-              editInput={editInput}
-              handleEditInput={handleEditInput}
-              saveTodo={saveTodo}
-              cancelEdit={cancelEdit}
-              editTodo={editTodo}
-              deleteTodo={deleteTodo}
-            />
-          ))}
-        </ul>
+      <div id="todo-list-container">
+        {pendingTodos.length > 0 && (
+          <div className="todo-container">
+            <h2 className="todo-list-title">Pending Todos</h2>
+            <ul>
+              {pendingTodos.map((item) => (
+                <TodoItem
+                  key={item.id}
+                  item={item}
+                  editingId={editingId}
+                  editInput={editInput}
+                  handleEditInput={handleEditInput}
+                  saveTodo={saveTodo}
+                  cancelEdit={cancelEdit}
+                  editTodo={editTodo}
+                  deleteTodo={deleteTodo}
+                  toggleCompleted={toggleCompleted}
+                />
+              ))}
+            </ul>
+          </div>
+        )}
+
+        {CompletedTodos.length > 0 && (
+          <div className="todo-container">
+            <h2 className="todo-list-title">Completed Todos</h2>
+            <ul>
+              {CompletedTodos.map((item) => (
+                <TodoItem
+                  key={item.id}
+                  item={item}
+                  editingId={editingId}
+                  editInput={editInput}
+                  handleEditInput={handleEditInput}
+                  saveTodo={saveTodo}
+                  cancelEdit={cancelEdit}
+                  editTodo={editTodo}
+                  deleteTodo={deleteTodo}
+                  toggleCompleted={toggleCompleted}
+                />
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
     </>
   );
--- react-app/src/Components/TodoList/TodoItem.jsx
+++ react-app/src/Components/TodoList/TodoItem.jsx
@@ -1,22 +1,63 @@
+import { useEffect, useRef } from "react";
 
 export default function TodoItem(props) {
+  const inputRef = useRef(null);
+  useEffect(() => {
+    if (props.editingId === props.item.id) {
+      inputRef.current?.focus();
+    }
+  }, [props.editingId]);
+
   return (
     <>
       <li>
         {props.item.id === props.editingId ? (
           <>
-            <input value={props.editInput} onChange={props.handleEditInput} />
+            <input
+              ref={inputRef}
+              value={props.editInput}
+              onChange={props.handleEditInput}
+            />
             <div className="todo-actions">
-              <button onClick={() => props.saveTodo(props.item.id)}>Save</button>
-              <button onClick={props.cancelEdit}>Cancel</button>
+              <button
+                className="save-edit-btn"
+                onClick={() => props.saveTodo(props.item.id)}
+              >
+                Save
+              </button>
+              <button className="delete-cancel-btn" onClick={props.cancelEdit}>
+                Cancel
+              </button>
+              <button
+                className="toggle-btn"
+                onClick={() => props.toggleCompleted(props.item.id)}
+              >
+                {props.item.isCompleted ? "←" : "→"}
+              </button>
             </div>
           </>
         ) : (
           <>
             <span>{props.item.value}</span>
             <div className="todo-actions">
-              <button onClick={() => props.editTodo(props.item)}>Edit</button>
-              <button onClick={() => props.deleteTodo(props.item.id)}>Delete</button>
+              { !props.item.isCompleted && <button
+                className="save-edit-btn"
+                onClick={() => props.editTodo(props.item)}
+              >
+                Edit
+              </button>}
+              <button
+                className="delete-cancel-btn"
+                onClick={() => props.deleteTodo(props.item.id)}
+              >
+                Delete
+              </button>
+              <button
+                className="toggle-btn"
+                onClick={() => props.toggleCompleted(props.item.id)}
+              >
+                {props.item.isCompleted ? "←" : "→"}
+              </button>
             </div>
           </>
         )}
