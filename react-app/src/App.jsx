504c40082a7f28a60fa1d63789dfc2baf6207ed7
Harsh Patel <harshpatel25121999@gmail.com>
2025-08-06 19:18:01 -0500

    Update tyhe branch with useContext and Reducers and Functional Todo App

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,15 +1,20 @@
 import ClassComponent from "./Components/ClassComponent/ClassComponent";
 import Game from "./Components/Tic Tac Toe/Game";
 import Todo from "./Components/TodoList/Todo";
+import { TodoProvider } from "./Components/TodoListUseContextandReducers/TodoContext";
 import TodoClass from "./Components/TodoListClassComponent/TodoClass";
+import TodoListContext from "./Components/TodoListUseContextandReducers/TodoListContext";
 
 function App() {
   return (
     <>
-    {/* <Todo /> */}
-    {/* <ClassComponent /> */}
-    {/* <TodoClass /> */}
-    <Game />
+      {/* <Todo /> */}
+      <TodoProvider>
+        <TodoListContext />
+      </TodoProvider>
+      {/* <ClassComponent /> */}
+      {/* <TodoClass /> */}
+      {/* <Game /> */}
     </>
   )
 }
